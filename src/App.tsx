e4aaf95f24a0679ef1dc2ac3dcbd92090de175f6
ELHOUATElmehdi <mehdielhouat3@gmail.com>
2023-11-04 17:59:49 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Profil } from './components/Profil';
+import { Informations_p } from './components/Informations_p';
+import { Photo } from './components/Photo';
+import { Intérêts } from './components/Intérêts';
+import { Formation } from './components/Formation';
+import { Compétences_ling } from './components/Compétences_ling';
+import { Compétences } from './components/Compétences';
+import { Compétences_tech } from './components/Compétences_tech';
+import { Description_de_l_objectif_professionnel } from './components/Description_de_l_objectif_professionnel';
+import { Titredelapage } from './components/Titredelapage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <Titredelapage />
+      <body>
+        <div className='cv-container'>  
+          <div className='cv-column1'>
+            <Photo />
+            <Header name='ELHOUAT' />
+            <Profil />
+            <Informations_p />
+            <Formation />
+          </div>
+          <div className='cv-column2'>
+            <Description_de_l_objectif_professionnel />
+            <Compétences />
+            <Compétences_tech />
+            <Compétences_ling />
+            <Intérêts />
+          </div>
+        </div>
+        <footer>
+          <Footer />
+        </footer>
+      </body>
+      </>
   );
 }
 
